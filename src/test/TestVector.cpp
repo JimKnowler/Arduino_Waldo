927637e8a6d98d09f919a191dae934cabcfc4831
Jim Knowler <jim@clockworkcamel.com>
2025-03-03 08:07:44 +0000

    Vector: add tests for setting size

--- src/test/TestVector.cpp
+++ src/test/TestVector.cpp
@@ -3,16 +3,11 @@
 #include "core/Vector.h"
 #include "core/String.h"
 
-// TODO: SetSize() after adding data - make sure that Used is reset to 0
-//  - when setting a larger size
-//  - when setting a smaller size
-//  - when setting the same size as current
-
 test(Vector, ShouldConstruct)
 {
     core::Vector<uint8_t> vector;
-    ensure(vector.Num() == 0);
-    ensure(vector.GetData() == nullptr);
+    assertEqual(vector.Num(), 0);
+    assertEqual(vector.GetData(), nullptr);
 }
 
 test(Vector, ShouldAddValue)
@@ -20,29 +15,29 @@ test(Vector, ShouldAddValue)
     core::Vector<uint8_t> vector;
     
     vector.Add(11);
-    ensure(vector.Num() == 1);
-    ensure(vector[0] == 11);
+    assertEqual(vector.Num(), 1);
+    assertEqual(vector[0], 11);
 
     vector.Add(22);
-    ensure(vector.Num() == 2);
-    ensure(vector[0] == 11);
-    ensure(vector[1] == 22);
+    assertEqual(vector.Num(), 2);
+    assertEqual(vector[0], 11);
+    assertEqual(vector[1], 22);
 
     vector.Add(33);
     vector.Add(44);
-    ensure(vector.Num() == 4);
-    ensure(vector[0] == 11);
-    ensure(vector[1] == 22);
-    ensure(vector[2] == 33);
-    ensure(vector[3] == 44);
+    assertEqual(vector.Num(), 4);
+    assertEqual(vector[0], 11);
+    assertEqual(vector[1], 22);
+    assertEqual(vector[2], 33);
+    assertEqual(vector[3], 44);
 
     vector.Add(55);
-    ensure(vector.Num() == 5);
-    ensure(vector[0] == 11);
-    ensure(vector[1] == 22);
-    ensure(vector[2] == 33);
-    ensure(vector[3] == 44);
-    ensure(vector[4] == 55);
+    assertEqual(vector.Num(), 5);
+    assertEqual(vector[0], 11);
+    assertEqual(vector[1], 22);
+    assertEqual(vector[2], 33);
+    assertEqual(vector[3], 44);
+    assertEqual(vector[4], 55);
 }
 
 test(Vector, ShouldAddValues)
@@ -57,20 +52,20 @@ test(Vector, ShouldAddValues)
 
     const uint8_t kValues[] = {1,2,3,4,5,6,7,8};
     vector.Add(kValues, 8);
-    ensure(vector.Num() == 13);
-    ensure(vector[0] == 11);
-    ensure(vector[1] == 22);
-    ensure(vector[2] == 33);
-    ensure(vector[3] == 44);
-    ensure(vector[4] == 55);
-    ensure(vector[5] == 1);
-    ensure(vector[6] == 2);
-    ensure(vector[7] == 3);
-    ensure(vector[8] == 4);
-    ensure(vector[9] == 5);
-    ensure(vector[10] == 6);
-    ensure(vector[11] == 7);
-    ensure(vector[12] == 8);
+    assertEqual(vector.Num(), 13);
+    assertEqual(vector[0], 11);
+    assertEqual(vector[1], 22);
+    assertEqual(vector[2], 33);
+    assertEqual(vector[3], 44);
+    assertEqual(vector[4], 55);
+    assertEqual(vector[5], 1);
+    assertEqual(vector[6], 2);
+    assertEqual(vector[7], 3);
+    assertEqual(vector[8], 4);
+    assertEqual(vector[9], 5);
+    assertEqual(vector[10], 6);
+    assertEqual(vector[11], 7);
+    assertEqual(vector[12], 8);
 }
 
 test(Vector, ShouldCopyConstruct)
@@ -80,13 +75,13 @@ test(Vector, ShouldCopyConstruct)
     vector.Add(kValues, 6);
 
     core::Vector<uint8_t> vector2(vector);
-    ensureEq(vector2.Num(), 6);
-    ensureEq(vector2[0], 8);
-    ensureEq(vector2[1], 7);
-    ensureEq(vector2[2], 6);
-    ensureEq(vector2[3], 5);
-    ensureEq(vector2[4], 4);
-    ensureEq(vector2[5], 3);
+    assertEqual(vector2.Num(), 6);
+    assertEqual(vector2[0], 8);
+    assertEqual(vector2[1], 7);
+    assertEqual(vector2[2], 6);
+    assertEqual(vector2[3], 5);
+    assertEqual(vector2[4], 4);
+    assertEqual(vector2[5], 3);
 }
 
 test(Vector, ShouldAssign)
@@ -97,20 +92,20 @@ test(Vector, ShouldAssign)
 
     core::Vector<uint8_t> vector2;
     vector2 = vector;
-    ensureEq(vector2.Num(), 5);
-    ensureEq(vector2[0], 8);
-    ensureEq(vector2[1], 7);
-    ensureEq(vector2[2], 6);
-    ensureEq(vector2[3], 5);
-    ensureEq(vector2[4], 4);
+    assertEqual(vector2.Num(), 5);
+    assertEqual(vector2[0], 8);
+    assertEqual(vector2[1], 7);
+    assertEqual(vector2[2], 6);
+    assertEqual(vector2[3], 5);
+    assertEqual(vector2[4], 4);
 }
 
 test(Vector, ShouldSetSize)
 {
     core::Vector<char> vector;
     vector.SetSize(32);
-    ensureEq(vector.Num(), 0);
-    ensure(vector.GetData() != nullptr);
+    assertEqual(vector.Num(), 0);
+    assertNotEqual(vector.GetData(), static_cast<char*>(nullptr));
 }
 
 test(Vector, ShouldSetSizeThenAddElements)
@@ -121,4 +116,31 @@ test(Vector, ShouldSetSizeThenAddElements)
     core::Vector<char> vector;
     vector.SetSize(Length);
     vector.Add(message.c_str(), Length);
+}
+
+test(Vector, ShouldSetSmallerSize)
+{
+    core::Vector<char> vector;
+    vector.SetSize(32);
+    vector.Add('a');
+    vector.SetSize(0);
+    assertEqual(vector.Num(), 0);
+}
+
+test(Vector, ShouldSetLargerSize)
+{
+    core::Vector<char> vector;
+    vector.SetSize(32);
+    vector.Add('a');
+    vector.SetSize(64);
+    assertEqual(vector.Num(), 0);
+}
+
+test(Vector, ShouldSetSameSize)
+{
+    core::Vector<char> vector;
+    vector.SetSize(32);
+    vector.Add('a');
+    vector.SetSize(32);
+    assertEqual(vector.Num(), 0);
 }
